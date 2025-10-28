8b5aba4463c95db5746d74a42bc0c83950e18a2a
Tawakol194 <tawakolalbhr@gmail.com>
2025-10-28 22:16:35 +0300

    added 3 file

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Apaartment_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtTenant = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.lblTenant = new System.Windows.Forms.Label();
+            this.lblRoom = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxNumber = new System.Windows.Forms.ComboBox();
+            this.lstApartments = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(984, 34);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(70, 17);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "رقم الشقة";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(836, 34);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(105, 24);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // txtTenant
+            // 
+            this.txtTenant.Location = new System.Drawing.Point(650, 78);
+            this.txtTenant.Name = "txtTenant";
+            this.txtTenant.Size = new System.Drawing.Size(291, 24);
+            this.txtTenant.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(778, 170);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(172, 24);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // lblTenant
+            // 
+            this.lblTenant.AutoSize = true;
+            this.lblTenant.Location = new System.Drawing.Point(960, 81);
+            this.lblTenant.Name = "lblTenant";
+            this.lblTenant.Size = new System.Drawing.Size(94, 17);
+            this.lblTenant.TabIndex = 5;
+            this.lblTenant.Text = "اسم المستأجر";
+            // 
+            // lblRoom
+            // 
+            this.lblRoom.AutoSize = true;
+            this.lblRoom.Location = new System.Drawing.Point(984, 130);
+            this.lblRoom.Name = "lblRoom";
+            this.lblRoom.Size = new System.Drawing.Size(67, 17);
+            this.lblRoom.TabIndex = 6;
+            this.lblRoom.Text = "عدد الغرف";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(1008, 170);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(43, 17);
+            this.lblPrice.TabIndex = 7;
+            this.lblPrice.Text = "الايجار";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(977, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "تاريخ الايجار";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(803, 214);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(147, 24);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // comboBoxNumber
+            // 
+            this.comboBoxNumber.FormattingEnabled = true;
+            this.comboBoxNumber.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBoxNumber.Location = new System.Drawing.Point(866, 127);
+            this.comboBoxNumber.Name = "comboBoxNumber";
+            this.comboBoxNumber.Size = new System.Drawing.Size(75, 24);
+            this.comboBoxNumber.TabIndex = 13;
+            // 
+            // lstApartments
+            // 
+            this.lstApartments.FormattingEnabled = true;
+            this.lstApartments.ItemHeight = 16;
+            this.lstApartments.Location = new System.Drawing.Point(12, 12);
+            this.lstApartments.Name = "lstApartments";
+            this.lstApartments.Size = new System.Drawing.Size(630, 468);
+            this.lstApartments.TabIndex = 14;
+            this.lstApartments.SelectedIndexChanged += new System.EventHandler(this.lstApartments_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(909, 310);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(108, 31);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "إضافة";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(758, 310);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(108, 31);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "حذف";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 484);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstApartments);
+            this.Controls.Add(this.comboBoxNumber);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblRoom);
+            this.Controls.Add(this.lblTenant);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtTenant);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtTenant;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblTenant;
+        private System.Windows.Forms.Label lblRoom;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBoxNumber;
+        private System.Windows.Forms.ListBox lstApartments;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apaartment_Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNumber.Text) || string.IsNullOrWhiteSpace(txtTenant.Text) ||
+                  string.IsNullOrWhiteSpace(txtPrice.Text) || comboBoxNumber.SelectedItem == null)
+            {
+                MessageBox.Show("يرجى تعبئة البيانات");
+            }
+            string Info = $"شقة رقم:{txtNumber.Text}    - المستأجر:{txtTenant.Text}   - " +
+                $"عدد الغرف: {comboBoxNumber.Text}    -   الايجار: {txtPrice.Text} ريال   - " +
+                $" التاريخ: {dateTimePicker1.Value.ToShortDateString()}";
+            lstApartments.Items.Add(Info);
+             
+            txtNumber.Clear();
+            txtPrice.Clear();
+            txtTenant.Clear();
+            comboBoxNumber.SelectedIndex = -1;
+        }
+
+        private void lstApartments_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstApartments.SelectedIndex == -1)
+            {
+                MessageBox.Show("يرجى اختيار عنصر للحذف!");
+            }
+            lstApartments.Items.RemoveAt(lstApartments.SelectedIndex);
+        }
+    }
+}
